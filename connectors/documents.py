fa7fe0d2d0e6125057fe30d79a5ca26cb6ede480
Oscar L. aka moas <lasm.landry@gmail.com>
2017-09-22 11:00:19 +0200

    fix pep

--- connectors/documents.py
+++ connectors/documents.py
@@ -10,48 +10,50 @@ from elasticsearch_dsl import Search
 DEFAULT_INDEX = 'index'
 DEFAULT_DOCTYPE = 'doctype'
 
+
 class DocumentConnector(object):
-	def __init__(self, index=DEFAULT_INDEX, doc_type=DEFAULT_DOCTYPE):
-		self.client = Elasticsearch()
-		self._index = index
-		self._doc_type = doc_type
+    def __init__(self, index=DEFAULT_INDEX, doc_type=DEFAULT_DOCTYPE):
+        self.client = Elasticsearch()
+        self._index = index
+        self._doc_type = doc_type
 
-	@property
-	def index(self):
-		return self._index
+    @property
+    def index(self):
+        return self._index
 
-	@property
-	def doc_type(self):
-		return self._doc_type
+    @property
+    def doc_type(self):
+        return self._doc_type
 
-	@property
-	def search_obj(self):
-		return Search(using=self.client, index=self.index)
+    @property
+    def search_obj(self):
+        return Search(using=self.client, index=self.index)
 
-	def add(self, body=None):
-		if not body:
-			with open('data/truite.json', 'r') as json_file:
-				body = json.load(json_file)
-		
-		res = self.client.index(
-			index=self.index, doc_type=self.doc_type, body=body
-		)
-		return res.get('_id', None)
+    def add(self, body=None):
+        if not body:
+            with open('data/truite.json', 'r') as json_file:
+                body = json.load(json_file)
 
-	def get_by_id(self, id_):
-		return self.client.get(
-			index=self.index, doc_type=self.doc_type, id=id_
-		)
+        res = self.client.index(
+            index=self.index, doc_type=self.doc_type, body=body
+        )
+        return res.get('_id', None)
 
-	def get_by_title(self, title):
-		return self.search_obj\
-			.query("match", title=title) \
-			.execute() \
-			.hits()
+    def get_by_id(self, id_):
+        return self.client.get(
+            index=self.index, doc_type=self.doc_type, id=id_
+        )
 
-	def delete_by_id(self, id_):
-		self.client.delete(
-			index=self.index, doc_type=self.doc_type, id=id_
-		)
-		
+    def get_by_title(self, title):
+        return self.search_obj \
+            .query("match", title=title) \
+            .execute() \
+            .hits()
+
+    def delete_by_id(self, id_):
+        self.client.delete(
+            index=self.index, doc_type=self.doc_type, id=id_
+        )
+
+
 doc = DocumentConnector()
